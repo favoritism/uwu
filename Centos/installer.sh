ae66ccea2ee99085c67fe478908fb52dafa1df4b
Tanner <tanner@atheist.com>
2020-11-04 16:32:10 -0600

    Update installer.sh

--- Centos/installer.sh
+++ Centos/installer.sh
@@ -19,13 +19,8 @@ yum install wget -y
 wget https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
 tar zxf softether-vpnserver*
 clear
-cd vpnserver && chmod 600 * && chmod 700 vpncmd && chmod 700 vpnserver && echo "Giving needed permissions to files in vpnserver/"
-ranlib /root/vpnserver/lib/libcharset.a && ranlib /root/vpnserver/lib/libcrypto.a && ranlib /root/vpnserver/lib/libedit.a && ranlib /root/vpnserver/lib/libiconv.a && ranlib /root/vpnserver/lib/libintelaes.a && ranlib /root/vpnserver/lib/libssl.a && ranlib /root/vpnserver/lib/libncurses.a && ranlib /root/vpnserver/lib/libz.a && echo "Ranlib..."
-ranlib /root/vpn*/code/vpnserver.a && echo "vpnserver.a"
-gcc code/vpnserver.a -O2 -fsigned-char -pthread -m64 -lm -ldl -lrt -lpthread -L./ /root/vpnserver/lib/libssl.a /root/vpnserver/lib/libcrypto.a /root/vpnserver/lib/libiconv.a /root/vpnserver/lib/libcharset.a /root/vpnserver/lib/libedit.a /root/vpnserver/lib/libncurses.a /root/vpnserver/lib/libz.a /root/vpnserver/lib/libintelaes.a -o vpnserver && echo "Compiling vpnserver..."
-ranlib /root/vpnserver/code/vpncmd.a && echo "vpncmd.a"
-gcc code/vpncmd.a -O2 -fsigned-char -pthread -m64 -lm -ldl -lrt -lpthread -L./ /root/vpnserver/lib/libssl.a /root/vpnserver/lib/libcrypto.a /root/vpnserver/lib/libiconv.a /root/vpnserver/lib/libcharset.a /root/vpnserver/lib/libedit.a /root/vpnserver/lib/libncurses.a /root/vpnserver/lib/libz.a /root/vpnserver/lib/libintelaes.a -o vpncmd && echo "Compiling vpncmd..."
-/root/vpnserver/vpncmd /tool /cmd:Check && echo "Check Done, if all passed then the vpn setup should work properly..."cd /root
+cd vpnserver && chmod 600 * && chmod 700 vpncmd && chmod 700 vpnserver
+make
 #Installing server command
 wget https://raw.githubusercontent.com/jaysonvelagio/SoftetherAutoInstallMultiPlatform/master/Centos/vpn-server.sh --no-check-certificate
 mv vpn-server.sh /etc/init.d/vpnserver
@@ -39,13 +34,8 @@ cd /root
 wget https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
 tar zxf softether-vpnclient*
 clear
-cd vpnclient && chmod 600 * && chmod 700 vpncmd && chmod 700 vpnclient && echo "Giving needed permissions to files in vpnclient/"
-ranlib /root/vpnclient/liblibcharset.a && ranlib /root/vpnclient/liblibcrypto.a && ranlib /root/vpnclient/liblibedit.a && ranlib /root/vpnclient/liblibiconv.a && ranlib /root/vpnclient/liblibintelaes.a && ranlib /root/vpnclient/liblibncurses.a && ranlib /root/vpnclient/liblibssl.a && ranlib /root/vpnclient/liblibz.a && ranlib /root/vpnclient/code/vpnclient.a && echo "Ranlib..."
-gcc /root/vpnclient/code/vpnclient.a -O2 -fsigned-char -pthread -m64 -lm -ldl -lrt -lpthread -L./ /root/vpnclient/liblibssl.a /root/vpnclient/liblibcrypto.a /root/vpnclient/liblibiconv.a /root/vpnclient/liblibcharset.a /root/vpnclient/liblibedit.a /root/vpnclient/liblibncurses.a /root/vpnclient/liblibz.a /root/vpnclient/liblibintelaes.a -o vpnclient && echo "Compiling vpnclient..."
-ranlib /root/vpnclient/code/vpncmd.a && echo "vpncmd.a"
-gcc /root/vpnclient/code/vpncmd.a -O2 -fsigned-char -pthread -m64 -lm -ldl -lrt -lpthread -L./ /root/vpnclient/liblibssl.a /root/vpnclient/liblibcrypto.a /root/vpnclient/liblibiconv.a /root/vpnclient/liblibcharset.a /root/vpnclient/liblibedit.a /root/vpnclient/liblibncurses.a /root/vpnclient/liblibz.a /root/vpnclient/liblibintelaes.a -o vpncmd && echo "Compiling vpncmd..."
-./vpnclient start && echo "Starting vpnclient..."
-cd /root/vpnclient/
+cd vpnclient && chmod 600 * && chmod 700 vpncmd && chmod 700 vpnclient
+make
 echo ---------------------------------------------
 echo  -e "\033[32;5mVPN SERVER INSTALLED SUCCESSFULLY!\033[0m"
 echo "SoftEther auto installer by JAYSON VELAGIO"
